01121768d1700694cd43add19a2c1e76cd0c1689
Krindendo work <lollipopfunkyx9@gmail.com>
2023-08-07 21:07:34 +0200

    new ui components and modifyed sidebar

--- components/docs-sidebar-nav.tsx
+++ components/docs-sidebar-nav.tsx
@@ -1,9 +1,39 @@
+import { usePathname } from "next/navigation"
 import { SidebarNavItem } from "@/types"
 
 export interface DocsSidebarNavProps {
   items: SidebarNavItem[]
 }
 
-export function DocsSidebarNav({}: DocsSidebarNavProps) {
-  return <p>Sidebar</p>
+export function DocsSidebarNav({ items }: DocsSidebarNavProps) {
+  const pathname = usePathname()
+
+  if (!items.length) return null
+
+  return (
+    <div className="w-full">
+      {items.map((item, index) => (
+        <div key={index} className="pb-8">
+          <h4>{item.title}</h4>
+          {item.items ? (
+            <DocsSidebarNavItem items={item.items} pathname={pathname} />
+          ) : null}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+interface DocsSidebarNavItemsProps {
+  items: SidebarNavItem[]
+  pathname: string | null
+}
+
+export function DocsSidebarNavItem({
+  items,
+  pathname,
+}: DocsSidebarNavItemsProps) {
+  if (!items?.length) return null
+
+  return <></>
 }
