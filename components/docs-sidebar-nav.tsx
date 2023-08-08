01817a351c51ab5c1f262e46e794cc26130d88ee
Krindendo work <lollipopfunkyx9@gmail.com>
2023-08-08 11:29:54 +0200

    added sidebar items

--- components/docs-sidebar-nav.tsx
+++ components/docs-sidebar-nav.tsx
@@ -1,5 +1,10 @@
+"use client"
+
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { SidebarNavItem } from "@/types"
+
+import { cn } from "@/lib/utils"
 
 export interface DocsSidebarNavProps {
   items: SidebarNavItem[]
@@ -35,5 +40,28 @@ export function DocsSidebarNavItem({
 }: DocsSidebarNavItemsProps) {
   if (!items?.length) return null
 
-  return <></>
+  return (
+    <div className="grid grid-flow-row auto-rows-max text-sm">
+      {items.map((item, index) =>
+        !item.disabled && item.href ? (
+          <Link
+            key={index}
+            href={item.href}
+            className={cn(
+              "flex w-full items-center rounded-md p-2 hover:underline",
+              {
+                "bg-muted": pathname === item.href,
+              }
+            )}
+          >
+            {item.title}
+          </Link>
+        ) : (
+          <span className="flex w-full items-center rounded-md p-2 opacity-60">
+            {item.title}
+          </span>
+        )
+      )}
+    </div>
+  )
 }
--- config/docs.ts
+++ config/docs.ts
@@ -15,35 +15,40 @@ export const docsConfig: DocsConfig = {
     {
       title: "Javascript",
       items: [
-        { title: "Introduction", href: "/docs/js", disabled: true },
-        { title: "Introduction", href: "/docs/js", disabled: true },
+        { title: "Introduction", href: "/docs/js" },
+        { title: "this", href: "/docs/js/this" },
       ],
     },
     {
       title: "Typescript",
       items: [
-        { title: "Introduction", href: "/docs/ts", disabled: true },
+        { title: "Introduction", href: "/docs/ts" },
         { title: "Introduction", href: "/docs/ts", disabled: true },
       ],
     },
     {
       title: "React",
       items: [
-        { title: "Introduction", href: "/docs/react", disabled: true },
-        { title: "Introduction", href: "/docs/react", disabled: true },
+        { title: "Introduction", href: "/docs/react" },
+        { title: "State Hooks", href: "/docs/react/state-hooks" },
+        { title: "Context Hooks", href: "/docs/react/context-hooks" },
+        { title: "Ref Hooks", href: "/docs/react/ref-hooks" },
+        { title: "Effect Hooks", href: "/docs/react/effect-hooks" },
+        { title: "Performance Hooks", href: "/docs/react/performance-hooks" },
+        { title: "Other Hooks", href: "/docs/react/other-hooks" },
       ],
     },
     {
       title: "Next.js",
       items: [
-        { title: "Introduction", href: "/docs/next", disabled: true },
+        { title: "Introduction", href: "/docs/next" },
         { title: "Introduction", href: "/docs/next", disabled: true },
       ],
     },
     {
       title: "Vue",
       items: [
-        { title: "Introduction", href: "/docs/vue", disabled: true },
+        { title: "Introduction", href: "/docs/vue" },
         { title: "Introduction", href: "/docs/vue/", disabled: true },
       ],
     },
