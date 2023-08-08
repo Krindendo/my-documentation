69a94d252f4709738cf55065e75259ab91fb0518
Krindendo work <lollipopfunkyx9@gmail.com>
2023-08-08 13:52:09 +0200

    refactored env

--- env.mjs
+++ env.mjs
@@ -2,18 +2,11 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  server: {
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),
-    GITHUB_ACCESS_TOKEN: z.string().min(1),
-  },
+  server: {},
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_VERCEL_URL: z.string().min(1),
   },
   runtimeEnv: {
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
   },
 })
--- lib/utils.ts
+++ lib/utils.ts
@@ -8,5 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`
+  let protocol = "http"
+
+  if (process.env.NODE_ENV === "production") {
+    protocol = "https"
+  }
+
+  return `${protocol}://${env.NEXT_PUBLIC_VERCEL_URL}${path}`
 }
