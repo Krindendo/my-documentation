5edd4e10448ddee458b6491e940c71c522aacfd8
Krindendo work <lollipopfunkyx9@gmail.com>
2023-08-21 20:38:14 +0200

    changed classNames in layouts

--- components/nav-mobile.tsx
+++ components/nav-mobile.tsx
@@ -37,7 +37,7 @@ export function NavMobile() {
           <Icons.logo className="mr-2 h-4 w-4" />
           <span className="font-bold">{siteConfig.name}</span>
         </MobileLink>
-        <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+        <ScrollArea className="mb-2 mt-4 h-[calc(100vh-5rem)] pl-6">
           <div className="flex flex-col space-y-3">
             {docsConfig.mainNav?.map(
               (item) =>
