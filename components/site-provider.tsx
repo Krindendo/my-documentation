10914d89f13592546630953170824b8440076196
Krindendo work <lollipopfunkyx9@gmail.com>
2023-09-18 23:37:19 +0200

    minor update

--- components/site-provider.tsx
+++ components/site-provider.tsx
@@ -3,15 +3,23 @@
 import * as React from "react"
 import { createStore, StoreApi, useStore } from "zustand"
 
+import { remToPx } from "@/lib/remToPx"
 import { TableOfContents } from "@/lib/toc"
+
+interface SectionHeader {
+  headingRef: React.MutableRefObject<React.JSX.Element>
+  id: string
+  offsetRem: number
+  title: string
+}
 
 interface SidebarState {
   sections: TableOfContents
-  setSections: (newSections: TableOfContents) => void
-  sectionIds: string[]
-  setSectionIds: (newSections: TableOfContents) => void
+  setSections: (sections: TableOfContents) => void
   visibleSections: string[]
   setVisibleSections: (visibleSections: string[]) => void
+  sectionIds: string[]
+  setSectionIds: (newSections: TableOfContents) => void
 }
 
 type SectionStore = ReturnType<typeof createSectionStore>
@@ -20,6 +28,8 @@ const createSectionStore = () => {
   return createStore<SidebarState>()((set) => ({
     sections: [],
     setSections: (sections) => set(() => ({ sections })),
+    visibleSections: [],
+    setVisibleSections: (visibleSections) => set(() => ({ visibleSections })),
     sectionIds: [],
     setSectionIds: (newSections) =>
       set(() => {
@@ -34,12 +44,8 @@ const createSectionStore = () => {
           .filter(Boolean)
           .map((id) => id?.split("#")[1])
 
-        const sectionWithTop = ["_top", ...sectionIds]
-
-        return { sectionIds: sectionWithTop as string[] }
+        return { sectionIds: ["_top", ...sectionIds] as string[] }
       }),
-    visibleSections: [],
-    setVisibleSections: (visibleSections) => set(() => ({ visibleSections })),
   }))
 }
 
@@ -83,40 +89,51 @@ const deleteSelector = (id: string) => ({
 
 type Action = ReturnType<typeof addSelector> | ReturnType<typeof deleteSelector>
 type State = {
-  selectors: string[]
+  visibleSections: string[]
 }
 
 const initialState: State = {
-  selectors: [],
+  visibleSections: [],
 }
 
 function reducer(state: State, action: Action) {
   switch (action.type) {
     case "ADD_SELECTOR": {
+      const sectionIndex = state.visibleSections.findIndex(
+        (section) => section === action.payload.id
+      )
+
+      if (sectionIndex !== -1) {
+        return { visibleSections: state.visibleSections }
+      }
+
       const index = action.payload.sectionIds.findIndex(
         (item) => item === action.payload.id
       )
+
       const firstIndex = action.payload.sectionIds.findIndex(
-        (item) => item === state.selectors[0]
+        (item) => item === state.visibleSections[0]
       )
 
       if (index === -1) {
-        return { selectors: state.selectors }
+        return { visibleSections: state.visibleSections }
       }
 
       if (index < firstIndex) {
         return {
-          selectors: [action.payload.id, ...state.selectors],
+          visibleSections: [action.payload.id, ...state.visibleSections],
         }
       }
 
       return {
-        selectors: [...state.selectors, action.payload.id],
+        visibleSections: [...state.visibleSections, action.payload.id],
       }
     }
     case "DELETE_SELECTOR": {
       return {
-        selectors: state.selectors.filter((s) => s !== action.payload.id),
+        visibleSections: state.visibleSections.filter(
+          (s) => s !== action.payload.id
+        ),
       }
     }
     default: {
@@ -127,34 +144,29 @@ function reducer(state: State, action: Action) {
 
 function useVisibleSections(sectionStore: StoreApi<SidebarState>) {
   const sectionIds = useStore(sectionStore, (s) => s.sectionIds)
+  const sections = useStore(sectionStore, (s) => s.sections)
   const setVisibleSections = useStore(sectionStore, (s) => s.setVisibleSections)
-
-  //console.log("sectionIds", sectionIds)
 
   const [activeIds, dispatchActiveIds] = React.useReducer<
     React.Reducer<State, Action>
   >(reducer, initialState)
 
   React.useEffect(() => {
+    console.log("activeIds", activeIds.visibleSections)
+  }, [activeIds])
+
+  React.useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            // console.log("add: ", entry.target.id)
-            dispatchActiveIds({
-              type: "ADD_SELECTOR",
-              payload: { id: entry.target.id, sectionIds },
-            })
-          } else {
-            // console.log("delete: ", entry.target.id)
-            dispatchActiveIds({
-              type: "DELETE_SELECTOR",
-              payload: { id: entry.target.id },
-            })
-          }
+          console.log("entry", entry.target.id)
         })
       },
-      { root: null, rootMargin: "0px 0px -30px 0px", threshold: 0.2 }
+      {
+        root: null,
+        rootMargin: "0px 0px -30px 0px",
+        threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+      }
     )
 
     sectionIds?.forEach((id) => {
@@ -181,7 +193,7 @@ function useVisibleSections(sectionStore: StoreApi<SidebarState>) {
   }, [sectionIds])
 
   React.useEffect(() => {
-    setVisibleSections(activeIds.selectors)
+    setVisibleSections(activeIds.visibleSections)
   }, [activeIds, setVisibleSections])
 }
 
@@ -197,3 +209,109 @@ export function InjectTOC({ toc }: { toc: TableOfContents }) {
 
   return null
 }
+
+/*
+          if (entry.isIntersecting) {
+            // console.log("add: ", entry.target.id)
+            dispatchActiveIds({
+              type: "ADD_SELECTOR",
+              payload: { id: entry.target.id, sectionIds },
+            })
+          } else {
+            // console.log("delete: ", entry.target.id)
+            dispatchActiveIds({
+              type: "DELETE_SELECTOR",
+              payload: { id: entry.target.id },
+            })
+          }
+
+
+*/
+
+/*
+    function checkVisibleSections() {
+      let { innerHeight, scrollY } = window
+      let newVisibleSections = []
+
+      for (
+        let sectionIndex = 0;
+        sectionIndex < sections.length;
+        sectionIndex++
+      ) {
+        let { id, headingRef, offsetRem } = sections[sectionIndex]
+        let offset = remToPx(offsetRem)
+        let top = headingRef.current.getBoundingClientRect().top + scrollY
+
+        if (sectionIndex === 0 && top - offset > scrollY) {
+          newVisibleSections.push("_top")
+        }
+
+        let nextSection = sections[sectionIndex + 1]
+        let bottom =
+          (nextSection?.headingRef.current.getBoundingClientRect().top ??
+            Infinity) +
+          scrollY -
+          remToPx(nextSection?.offsetRem ?? 0)
+
+        if (
+          (top > scrollY && top < scrollY + innerHeight) ||
+          (bottom > scrollY && bottom < scrollY + innerHeight) ||
+          (top <= scrollY && bottom >= scrollY + innerHeight)
+        ) {
+          newVisibleSections.push(id)
+        }
+      }
+
+      setVisibleSections(newVisibleSections)
+    }
+
+*/
+
+/*
+          const { innerHeight, scrollY } = window
+
+          const top = entry.boundingClientRect.top + scrollY
+
+          const indexOfPreviousSection = sectionIds.findIndex(
+            (section) => section === entry.target.id
+          )
+
+          let previousSection = sectionIds[indexOfPreviousSection + 1]
+
+          const bottom =
+            document.getElementById(previousSection)?.getBoundingClientRect()
+              .top ?? Infinity + scrollY
+
+          if (entry.target.id === "_top") {
+            if (top > scrollY) {
+              dispatchActiveIds({
+                type: "ADD_SELECTOR",
+                payload: { id: "_top", sectionIds },
+              })
+            } else {
+              dispatchActiveIds({
+                type: "DELETE_SELECTOR",
+                payload: { id: "_top" },
+              })
+            }
+          } else {
+            if (
+              (top > scrollY && top < scrollY + innerHeight) ||
+              (bottom > scrollY && bottom < scrollY + innerHeight) ||
+              (top <= scrollY && bottom >= scrollY + innerHeight)
+            ) {
+              console.log("add: ", entry.target.id)
+              dispatchActiveIds({
+                type: "ADD_SELECTOR",
+                payload: { id: entry.target.id, sectionIds },
+              })
+            } else {
+              console.log("delete: ", entry.target.id)
+              dispatchActiveIds({
+                type: "DELETE_SELECTOR",
+                payload: { id: entry.target.id },
+              })
+            }
+          }
+
+*/
