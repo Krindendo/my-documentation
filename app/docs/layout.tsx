7c475a316e7194ec6d2cbca507d4e0a65ab6cb6c
Krindendo work <lollipopfunkyx9@gmail.com>
2023-10-04 20:00:05 +0200

    update docs

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,5 +3,5 @@ interface DocsLayoutProps {
 }
 
 export default function DocsLayout({ children }: DocsLayoutProps) {
-  return <div className="container flex flex-1 items-start">{children}</div>
+  return <div className="container flex-1">{children}</div>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
+const { fontFamily, screens } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -14,8 +14,15 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "2rem",
+      },
       screens: {
+        ...screens,
         "2xl": "1400px",
       },
     },
