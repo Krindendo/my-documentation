8f4b6753823b487567e997807b2506da7abf67cf
Krindendo work <lollipopfunkyx9@gmail.com>
2023-10-29 00:27:39 +0200

    fix: route error

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,4 +1,5 @@
-import { ImageResponse, NextRequest } from "next/server"
+import { ImageResponse } from "next/og"
+import { NextRequest } from "next/server"
 
 import { siteConfig } from "@/config/site"
 import { ogImageSchema } from "@/lib/validations/og"
