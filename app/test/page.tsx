00e06a681ce88aabbd60c3bd5da77ac84a83747f
Krindendo work <lollipopfunkyx9@gmail.com>
2023-12-29 00:27:05 +0100

    to check

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -9,3 +9,23 @@ import { cn } from "@/lib/utils"
 export default function Page() {
   return <></>
 }
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length > 0) {
+      mutation.addedNodes.forEach((node) => {
+        if (node.className === "some-class") {
+          console.log("node", node)
+        }
+      })
+    }
+  })
+})
+
+const target = document.querySelector("#nesto")
+const options = {
+  childList: true,
+  subtree: true,
+}
+
+observer.observe(target, options)
