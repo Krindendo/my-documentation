6d5951a1e3e973601b35cbb304be67c34f704cde
Krindendo work <lollipopfunkyx9@gmail.com>
2024-08-03 15:59:00 +0200

    feat: change logic for withSidebar

--- components/withSidebar.tsx
+++ components/withSidebar.tsx
@@ -2,26 +2,29 @@
 
 import type { FC } from 'react';
 
-import Sidebar from '@/components/Containers/Sidebar';
+import { Sidebar } from '@/components/Containers/Sidebar';
 import { useSiteNavigation } from '@/hooks/server';
 import { useSectionStore } from '@/providers/sidebarProvider';
 import { useStore } from 'zustand';
+import { sideNavigation } from '@/config/navigation';
 
 interface WithSidebarProps {
   context?: Record<string, string>;
 }
 
-const WithSidebar: FC<WithSidebarProps> = ({ context }) => {
+const WithSidebar: FC<WithSidebarProps> = () => {
   const store = useSectionStore();
   const sectionPath = useStore(store, s => s.sectionPath);
-  const { getSideNavigation } = useSiteNavigation();
+  //const { getSideNavigation } = useSiteNavigation();
 
-  const mappedSidebarItems = getSideNavigation([sectionPath], context).map(
-    ([, { label, items }]) => ({
-      groupName: label,
-      items: items.map(([, item]) => item),
-    })
-  );
+  // const mappedSidebarItems = getSideNavigation([sectionPath], context).map(
+  //   ([, { label, items }]) => ({
+  //     groupName: label,
+  //     items: items.map(([, item]) => item),
+  //   })
+  // );
+
+  const mappedSidebarItems = sideNavigation[sectionPath].items ?? [];
 
   return <Sidebar groups={mappedSidebarItems} />;
 };
