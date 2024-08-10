8e1720ff392aba33a953d22203adfb91ff368000
Krindendo work <lollipopfunkyx9@gmail.com>
2024-08-10 18:21:11 +0200

    chore: removed rewrite config

--- next.json.mjs
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import _siteRedirects from './redirects.json' assert { type: 'json' };
-
-/** @type {Record<string, Array<import('./types').Redirect>>} */
-export const siteRedirects = _siteRedirects;
