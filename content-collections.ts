df9f92e7af9d424489cec8fbb68b84b2cee9263f
Krindendo work <lollipopfunkyx9@gmail.com>
2024-12-04 23:17:04 +0100

    add public folder

--- content-collections.ts
+++ content-collections.ts
@@ -1,15 +1,46 @@
 import { defineCollection, defineConfig } from "@content-collections/core";
+import { compileMDX } from "@content-collections/mdx";
+import {
+  type RehypeCodeOptions,
+  rehypeCode,
+  remarkGfm,
+  remarkHeading,
+} from "fumadocs-core/mdx-plugins";
 
-const posts = defineCollection({
-  name: "posts",
-  directory: "src/posts",
-  include: "**/*.md",
+const rehypeCodeOptions: RehypeCodeOptions = {
+  themes: {
+    light: "catppuccin-mocha",
+    dark: "catppuccin-mocha",
+  },
+};
+
+const docs = defineCollection({
+  name: "docs",
+  directory: "content/docs",
+  include: "**/*.mdx",
   schema: (z) => ({
-    title: z.string(),
-    summary: z.string(),
+    title: z.string().min(1),
+    description: z.string().min(1),
+    date: z.string().min(1),
+    published: z.boolean(),
   }),
+  transform: async (page, context) => {
+    const body = await context.cache(page.content, async () =>
+      compileMDX(context, page, {
+        remarkPlugins: [remarkGfm, remarkHeading],
+        rehypePlugins: [[rehypeCode, rehypeCodeOptions]],
+      })
+    );
+
+    return {
+      ...page,
+      body,
+      date: new Date(page.date),
+      slug: page._meta.path,
+    };
+  },
 });
 
 export default defineConfig({
-  collections: [posts],
+  collections: [docs],
 });
