68547a8b9b058282abfc710a3d6754f8449d8f93
Krindendo work <lollipopfunkyx9@gmail.com>
2024-12-15 01:14:30 +0100

    test

--- app/algorithms/[[...slug]]/page.tsx
+++ app/algorithms/[[...slug]]/page.tsx
@@ -34,25 +34,30 @@ export const generateStaticParams = (): { slug?: string[] }[] =>
   }));
 
 const AlgorithmsPage = async ({ params }: AlgorithmsPageProperties) => {
+  const slug = (await params).slug;
   const path = (await params).slug?.join("\\") || "";
   const page = allAlgorithms.find(({ slugAsPath }) => slugAsPath === path);
 
   if (!page) {
-    notFound();
+    //notFound();
   }
 
   return (
-    <div className="container py-16">
-      <PageHeader heading={page.title} text={page.description} />
-      <div className="mt-16 flex flex-col items-start gap-8 sm:flex-row">
-        <div className="sm:flex-1">
-          <Mdx code={page.body} />
-        </div>
-        <div className="sticky top-24 hidden shrink-0 md:block">
-          <TableOfContents content={page.content} date={page.date} />
-        </div>
-      </div>
+    <div>
+      slug: {slug}
+      path: {path}
     </div>
+    // <div className="container py-16">
+    //   <PageHeader heading={page.title} text={page.description} />
+    //   <div className="mt-16 flex flex-col items-start gap-8 sm:flex-row">
+    //     <div className="sm:flex-1">
+    //       <Mdx code={page.body} />
+    //     </div>
+    //     <div className="sticky top-24 hidden shrink-0 md:block">
+    //       <TableOfContents content={page.content} date={page.date} />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
