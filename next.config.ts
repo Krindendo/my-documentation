07a3a3268e8d88d8650a06e8916688304a4877c4
Krindendo work <lollipopfunkyx9@gmail.com>
2025-04-08 21:14:44 +0200

    add react compiler

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import { withContentCollections } from "@content-collections/next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default withContentCollections(nextConfig);
